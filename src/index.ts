e5d6d02d5f6586d4ab5a2e2511b57f3169ef56b5
ankitkumarbarik <barikankitkumar2@gmail.com>
2026-04-02 16:37:26 +0530

    basic express code setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello');
+import { createServer } from "node:http";
+import { createExpressApplication } from "./app";
+
+async function main() {
+    try {
+        const server = createServer(createExpressApplication());
+        const PORT = process.env.PORT ? +process.env.PORT : 5000;
+        server.listen(PORT, () =>
+            console.log(`Http server is running on PORT:${PORT}`),
+        );
+    } catch (error) {
+        console.log(`Error starting http server`);
+        throw error;
+    }
+}
+
+main();
